43f78b6d6b778c44df849e80baa1ab5b29140e2b
Aaron Burkett <burkett3@WIN.ecf.utoronto.ca>
2023-10-22 18:14:59 -0400

    Updated user entity to include more attributes.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO users(email, password) VALUES('kevinjames@mallcop.com', 'segway');
-INSERT INTO users(email, password) VALUES('djkhaled@gmail.com', 'anothaOne');
-INSERT INTO users(email, password) VALUES('kanye@west.org', 'buyDaEarf123');
-INSERT INTO users(email, password) VALUES('lebron@king.net', 'Strive4Gr8ness');
-INSERT INTO users(email, password) VALUES('aubreyg@yahoo.com', 'money4fun');
+INSERT INTO users(email, password, firstName, lastName) VALUES('kevinjames@mallcop.com', 'segway', 'Kevin', 'James');
+INSERT INTO users(email, password, firstName, lastName) VALUES('djkhaled@gmail.com', 'anothaOne', 'DJ', 'Khaled');
+INSERT INTO users(email, password, firstName, lastName) VALUES('kanye@west.org', 'buyDaEarf123', 'Kanye', 'West');
+INSERT INTO users(email, password, firstName, lastName) VALUES('lebron@king.net', 'Strive4Gr8ness', 'LeBron', 'James');
+INSERT INTO users(email, password, firstName, lastName) VALUES('aubreyg@yahoo.com', 'money4fun', 'Aubrey', 'Graham');
