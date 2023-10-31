9696b051aa9a509d4426493ba4cbe2df488328f0
Joshua Lee <joshualee@Joshuas-MacBook-Air.local>
2023-10-31 16:47:46 -0400

    added annotators to vehicle controller

--- src/main/java/com/example/RideShare/controller/VehicleController.java
+++ src/main/java/com/example/RideShare/controller/VehicleController.java
@@ -2,7 +2,13 @@ package com.example.RideShare.controller;
 
 import com.example.RideShare.model.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
+@RestController
+@RequestMapping("/vehicles")
 public class VehicleController {
 
     @Autowired
