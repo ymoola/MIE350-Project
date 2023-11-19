94e73721d91fe477912d53e4c45ef14876281db4
Joshua Lee <joshualee@Joshuas-MacBook-Air.local>
2023-11-18 22:16:17 -0500

    instructions and class instantiation

---
+++ src/main/java/com/example/RideShare/geocoding/Geocoder.java
@@ -0,0 +1,7 @@
+package com.example.RideShare.geocoding;
+
+public class Geocoder {
+    public Geocoder() {}
+
+    //make function that calls a geocoding api to go from address to (latitude, longitude) coordinates
+}
