fb464420b721b32ea0070b8e452a24133adaaf80
Aaron Burkett <burkett3@WIN.ecf.utoronto.ca>
2023-11-20 17:40:57 -0500

    Added Geocoder class to convert address to coordinates

--- src/main/java/com/example/RideShare/geocoding/Geocoder.java
+++ src/main/java/com/example/RideShare/geocoding/Geocoder.java
@@ -1,7 +1,39 @@
 package com.example.RideShare.geocoding;
 
-public class Geocoder {
-    public Geocoder() {}
 
-    //make function that calls a geocoding api to go from address to (latitude, longitude) coordinates
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+//App ID: hQYGf8MkYoytWpKcNVah
+//API key: jjeVTW3p62ycAM7w1ysCqmfZGXSU67j65DXytVaoVio
+
+public class Geocoder {
+
+    private static final String GEOCODING_RESOURCE = "https://geocode.search.hereapi.com/v1/geocode";
+    private static final String API_KEY = "jjeVTW3p62ycAM7w1ysCqmfZGXSU67j65DXytVaoVio";
+
+    public static String GeocodeSync(String query) throws IOException, InterruptedException {
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        String encodedQuery = URLEncoder.encode(query,"UTF-8");
+        String requestUri = GEOCODING_RESOURCE + "?apiKey=" + API_KEY + "&q=" + encodedQuery;
+
+        HttpRequest geocodingRequest = HttpRequest.newBuilder().GET().uri(URI.create(requestUri))
+                .timeout(Duration.ofMillis(2000)).build();
+
+        HttpResponse geocodingResponse = httpClient.send(geocodingRequest,
+                HttpResponse.BodyHandlers.ofString());
+
+        return (String) geocodingResponse.body();
+    }
+
 }
