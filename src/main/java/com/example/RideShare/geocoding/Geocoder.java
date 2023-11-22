edbfe626f64f14f5fbaa44fa59ab869b53c174d7
Aaron Burkett <burkett3@WIN.ecf.utoronto.ca>
2023-11-22 13:11:02 -0500

    w commit

--- src/main/java/com/example/RideShare/geocoding/Geocoder.java
+++ src/main/java/com/example/RideShare/geocoding/Geocoder.java
@@ -20,20 +20,32 @@ public class Geocoder {
     private static final String GEOCODING_RESOURCE = "https://geocode.search.hereapi.com/v1/geocode";
     private static final String API_KEY = "jjeVTW3p62ycAM7w1ysCqmfZGXSU67j65DXytVaoVio";
 
-    public static String GeocodeSync(String query) throws IOException, InterruptedException {
+    /**
+     *
+     * @param query
+     * @return json object of coordinates with keys lng, lat
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public static JSONObject GeocodeSync(String query) throws IOException, InterruptedException {
 
         HttpClient httpClient = HttpClient.newHttpClient();
 
         String encodedQuery = URLEncoder.encode(query,"UTF-8");
         String requestUri = GEOCODING_RESOURCE + "?apiKey=" + API_KEY + "&q=" + encodedQuery;
-
         HttpRequest geocodingRequest = HttpRequest.newBuilder().GET().uri(URI.create(requestUri))
                 .timeout(Duration.ofMillis(2000)).build();
 
         HttpResponse geocodingResponse = httpClient.send(geocodingRequest,
                 HttpResponse.BodyHandlers.ofString());
 
-        return (String) geocodingResponse.body();
+        Object arg = geocodingResponse.body();
+        JSONObject responseObject = new JSONObject((String) arg);
+        JSONArray items = responseObject.getJSONArray("items");
+        JSONObject itemsFirst = (JSONObject) items.get(0);
+        JSONObject coordinates = itemsFirst.getJSONObject("position");
+
+        return coordinates;
     }
 
 }
