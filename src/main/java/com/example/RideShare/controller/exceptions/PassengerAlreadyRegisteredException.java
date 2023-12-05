3ee86ba432e88a89b1052ca81670e300c383dce1
Joshua Lee <joshualee@Joshuas-MacBook-Air.local>
2023-12-05 17:38:42 -0500

    finished trip request tests

--- src/main/java/com/example/RideShare/controller/exceptions/PassengerAlreadyRegisteredException.java
+++ src/main/java/com/example/RideShare/controller/exceptions/PassengerAlreadyRegisteredException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
-public class PassangerAlreadyRegisteredException extends RuntimeException {
-    public PassangerAlreadyRegisteredException (long tripId, String email){
+public class PassengerAlreadyRegisteredException extends RuntimeException {
+    public PassengerAlreadyRegisteredException(long tripId, String email){
         super(String.format("User with email %s already registered for trip (ID: %d).", email, tripId));
 
     }
